e56842f2560bfffec94a3bc1adf6398e916af889
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-06 14:34:54 -0300

    Adicionando Pessoa/add-pessoa.ts

--- src/app/page/add-pessoa/add-pessoa.page.ts
+++ src/app/page/add-pessoa/add-pessoa.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from "@ionic/angular";
 
 @Component({
   selector: 'app-add-pessoa',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPessoaPage implements OnInit {
 
-  constructor() { }
+  id:string;
+  pessoas: any = [];
+  pessoa = {
+    id: null,
+    nome: null,
+    dataNascimento: null,
+    cpf: null,
+    telefone:null,
+    cep: null
+  };
+
+  constructor(private activatedRoute: ActivatedRoute, private navController: NavController) { }
 
   ngOnInit() {
+    this.pessoas = JSON.parse(localStorage.getItem("pessoaBD"));
+    if(!this.pessoas){
+      this.pessoas = [];
+      localStorage.setItem("pessoaBD", JSON.stringify(this.pessoas));
+    }
+
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(this.id){
+      this.pessoas[this.id] = this.pessoa;
+    }else{
+      this.pessoas.push(this.pessoa);
+    }
   }
 
+  async submitForm(){
+    this.pessoas = JSON.parse(localStorage.getItem("pessoaBD"));
+
+    if(this.id){
+      this.pessoas[this.id] = this.pessoa;
+    }else{
+      this.pessoas.push(this.pessoa);
+    }
+
+    localStorage.setItem("pessoaBD", JSON.stringify(this.pessoas));
+    this.navController.navigateBack('/pessoa');
+  }
 }
