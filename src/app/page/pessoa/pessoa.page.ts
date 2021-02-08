e23f909a4ce9f21deee42c712d8cc0740189206e
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-08 15:39:39 -0300

    exclusao Pessoa

--- src/app/page/pessoa/pessoa.page.ts
+++ src/app/page/pessoa/pessoa.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-pessoa',
@@ -8,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class PessoaPage implements OnInit {
 
   pessoas: any = [];
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, public alertController: AlertController, private navController: NavController) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe( param => {
+      if(param['id']){
+        this.pessoas = JSON.parse(localStorage.getItem("pessoaBD"));
+        for(var i = 0; i < this.pessoas.length; i++){
+          if(this.pessoas[i].id === param['id']){
+            this.pessoas.splice(i, 1);
+            localStorage.setItem("pessoaBD", JSON.stringify(this.pessoas));
+            this.mostrarAlerta("Excluida");
+          }
+        }
+      }
+    });
+  }
+
+  async mostrarAlerta(parametro: string){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      message: 'Pessoa ' + parametro + ' Com Sucesso!',
+      buttons: ['OK'] 
+    });
+
+    await alert.present();
   }
 
   async ionViewWillEnter(){
